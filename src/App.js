5a9d2a343073625fd843bc585dd076250220b9ab
alexpod <alexpoddubnyy@gmail.com>
2024-07-15 23:07:44 +0500

    task hover

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {  } from 'react';
+import './App.css';
+
+function App() {
+  const { hovered, ref } = useHover();
+
+  return (
+    <div ref={ref}>
+      {hovered ? 'На меня навели мышку' : 'Наведи мышкой на меня'}
+    </div>
+  );
+}
+
+export default App;
