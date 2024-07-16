d1de1ea1f2a8093e5540a9e091ca792e0df3b542
alexpod <alexpoddubnyy@gmail.com>
2024-07-16 16:47:05 +0500

    window scroll

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, {  } from 'react';
+import './App.css';
+import { useWindowScroll } from './hooks/useWindowScroll';
+
+function App() {
+  const [scroll, scrollTo] = useWindowScroll();
+
+
+  return (
+    <div className='panel'>
+      <p>
+        Scroll position x: {scroll.x}, y: {scroll.y}
+      </p>
+      <button onClick={() => scrollTo({ y: 0 })}>Scroll to top</button>
+    </div>
+  );
+}
+
+export default App;
+
