e9b8c0438d8667aa8fb8c6b57bf59a98b77e66e3
alexpod <alexpoddubnyy@gmail.com>
2024-08-07 17:37:57 +0500

    project Rick and morty

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './assets/scss/index.scss'
+import './assets/scss/layout.scss'
+
+function App() {
+
+  return (
+    <>
+      <h1>Rick and Morty</h1>
+    </>
+  )
+}
+
+export default App
